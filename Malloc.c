021470603fab82a435c400e27275acde9b463f0d
mdehoogh <m.p.a.j.dehoogh@tudelft.nl>
2019-06-21 13:49:39 +0200

    and the source files as well now (Malloc.h/c)

---
+++ Malloc.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+// helper functions
+// we need a local something to store the allocation types in
+struct {
+    size_t l; // the number of allocation types stored
+    char* chars; // the allocation type characters
+}allocationtypes;
+
+// you HAVE to call this method to be able to register allocations
+bool allocationrecordinginitialized(){
+    allocationtypes.l=0;
+#ifdef __ADEBUG__
+    allocationtypes.chars=malloc(16); // starting out with one block
+#else
+    allocationtypes.chars=NULL;
+#endif
+    return(allocationtypes.chars!=NULL);
+    // replacing: if(!allocationtypes.chars)printf("ERROR: Failed to initialize recording allocations.\n");else printf("Allocation recording initialized.\n");
+}
+
+size_t addallocationtype(char allocationtype){
+#ifdef __ADEBUG__
+    // increase size if necessary
+    if(allocationtypes.chars&&!(allocationtypes.l&0xF))allocationtypes.chars=realloc(allocationtypes.chars,(allocationtypes.l+16)); // add a 'block' if now full
+    if(!allocationtypes.chars)return 0;
+    allocationtypes.chars[allocationtypes.l++]=allocationtype;
+    return allocationtypes.l;
+#else
+    return 0;
+#endif
+}
+
+// 'public' functions
+size_t allocationmark(){return addallocationtype(' ');}
+
+// unmark allocation returns the total number of encountered allocations
+size_t unmarkallocation(size_t mark){
+#ifdef __ADEBUG__
+    if(!allocationtypes.chars){printf("No allocation recording!\n");return 0;}
+    if(mark==0){printf("No mark!\n");return 0;}
+    if(mark>allocationtypes.l){printf("Mark %zu too large.\n",mark);return 0;}
+    if(allocationtypes.chars[mark-1]!=' '){printf("No mark at position %zu!\n",mark);return 0;}
+    allocationtypes.l=mark-1;
+#endif
+    return allocationtypes.l; // returning what's left!!!
+}
+void allocationreport(size_t mark){
+#ifdef __ADEBUG__
+    if(mark!=0&&allocationtypes.chars!=NULL){
+        printf("%s","Allocations: '");
+        size_t pos=mark-1;
+        while(pos<allocationtypes.l)printf("%c",allocationtypes.chars[pos++]);
+        printf("%c",'\'');
+    }else
+        printf("%s","No allocations.");
+    printf("\n");
+#endif
+}
+void syncallocations(){
+#ifdef __ADEBUG__
+    if(!allocationtypes.chars)return;
+    while(allocationtypes.l>0){if(allocationtypes.chars[allocationtypes.l-1]!='.')break;allocationtypes.l--;}
+    allocationreport(1);
+#endif
+}
+
+#ifdef __ADEBUG__
+void* Mmalloc(size_t size,char type){
+    void* ptr=malloc(size);
+    if(ptr)addallocationtype(type);
+    return ptr;
+}
+
+void* Mcalloc(size_t nitems,size_t size,char type){
+    void* ptr=calloc(nitems,size);
+    if(ptr)addallocationtype(type);
+    return ptr;
+}
+
+void Mfree(void* ptr,char type){
+    if(!ptr)return;
+    free(ptr);
+    // undo the allocation of the given type
+    if(!allocationtypes.chars){printf("Allocation types not recorded!\n");return;}
+    if(allocationtypes.l==0){printf("Nothing allocated to free.\n");return;}
+    size_t pos=allocationtypes.l-1;
+    while(1){
+        if(allocationtypes.chars[pos]==' '){printf("Allocation of type '%c' not encountered.\n",type);break;}
+        if(allocationtypes.chars[pos]==type){allocationtypes.chars[pos]='.';break;}
+        if(pos==0)break;
+        pos--;
+    }
+}
+#endif
---
+++ Malloc.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+// a user can mark the allocation by calling Mmark() and using the returned position to unmark
+// typically all unmark calls should unmark the most recent mark (otherwise an unmark is missing)
+size_t addallocationtype(char allocationtype);
+bool allocationrecordinginitialized();
+size_t allocationmark();
+size_t unmarkallocation(size_t mark);
+void allocationreport(size_t mark); // report on the current allocation status
+void syncallocations();
+
+#ifdef __ADEBUG__
+void* Mmalloc(size_t size,char type);
+void* Mcalloc(size_t nitems,size_t size,char type);
+void Mfree(void* ptr,char type);
+// use the substitutes
+#define MALLOC(size,type) Mmalloc(size,type)
+#define CALLOC(nitems,size,type) Mcalloc(nitems,size,type)
+#define FREE(ptr,type) Mfree(ptr,type)
+#else
+// use the system methods
+#define MALLOC(size,type) malloc(size)
+#define CALLOC(nitems,size,type) calloc(nitems,size)
+#define FREE(ptr,type) free(ptr)
+#endif
