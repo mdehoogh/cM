8251c429c6e8c831d87ec79bca92cc8b4522eacf
mdehoogh <m.p.a.j.dehoogh@tudelft.nl>
2022-11-07 16:06:43 +0100

    MConfig.h added

---
+++ src/MConfig.h
@@ -0,0 +1,8 @@
+// the configured options and settings for M
+#define M_VERSION_MAJOR "0"
+#define M_VERSION_MINOR "1"
+#define M_VERSION_PATCH "5"
+#define M_VERSION_TWEAK ""
+#define M_VERSION_BUILD "12"
+#define M_VERSION_DATE "07 November 2022"
+#define M_BUILD_TIMESTAMP "07-11-2022 16:00:00"
