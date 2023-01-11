91e9c55839784b3bede1921ec4c26b704e28fa5b
mdehoogh <m.p.a.j.dehoogh@tudelft.nl>
2023-01-11 09:50:48 +0100

    New branch started

--- src/MConfig.h
+++ src/MConfig.h
@@ -1,4 +1,5 @@
 // the configured options and settings for M
+/*
 #define M_VERSION_MAJOR "0"
 #define M_VERSION_MINOR "1"
 #define M_VERSION_PATCH "5"
@@ -6,3 +7,12 @@
 #define M_VERSION_BUILD "13"
 #define M_VERSION_DATE "20 December 2022"
 #define M_BUILD_TIMESTAMP "20-12-2022 14:00:00"
+*/
+// MDH@11JAN2023: new branch version started to accomodate for advanced memory management features
+#define M_VERSION_MAJOR "0"
+#define M_VERSION_MINOR "1"
+#define M_VERSION_PATCH "6"
+#define M_VERSION_TWEAK ""
+#define M_VERSION_BUILD "1"
+#define M_VERSION_DATE "11 January 2023"
+#define M_BUILD_TIMESTAMP "11-01-2023 10:00:00"
